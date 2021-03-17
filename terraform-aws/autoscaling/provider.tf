c8451bf23dac57d567bb542cca5135b4f831b69e
root <root@MAVLT1219.mavericsystems.lan>
2021-03-17 22:36:32 +0530

    Added autoscaling group tf

---
+++ terraform-aws/autoscaling/provider.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+  region = var.AWS_REGION
+  access_key = "XXXXXXXXXX"
+  secret_key = "XXXXXXX"
+}
+
--- terraform-aws/s3withiam/provider.tf
+++ terraform-aws/s3withiam/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = var.AWS_REGION
-  access_key = "AKIA35446ZIQC4TZHBP2"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
 
